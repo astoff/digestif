505ca88a54012be5534654238baaa457f8446fed
Jens Schneider <jens.schneider.ac@posteo.de>
2021-10-06 21:27:45 +0200

    Add pgf/tikz docstrings

--- scripts/extract-pgfmanual.lua
+++ scripts/extract-pgfmanual.lua
@@ -1,7 +1,7 @@
 if not arg[1] then
    print("Usage: " .. arg[0] .. [[ PATH [--dump]
 
-PATH should point to the base of the pgf distribution (so that PATH/doc exists)
+PATH should point to the 'text-en' directory of the pgf distribution
 ]])
    return
 end
@@ -12,14 +12,8 @@ P, V, R, S, B = lpeg.P, lpeg.V, lpeg.R, lpeg.S, lpeg.B
 C, Cp, Cs, Cmt, Cf, Ct = lpeg.C, lpeg.Cp, lpeg.Cs, lpeg.Cmt, lpeg.Cf, lpeg.Ct
 Cc, Cg = lpeg.Cc, lpeg.Cg
 
-save_from_table = require"luarocks.persist".save_from_table
-load_into_table = require"luarocks.persist".load_into_table
-
 util = require "digestif.util"
 lfs = require "lfs"
-ser = require "serpent".block
-dump = require "serpent".dump
-see = require "see"
 concat = table.concat
 merge = util.merge
 search, gobble_until, case_fold = util.search, util.gobble_until, util.case_fold
@@ -28,6 +22,56 @@ choice, sequence = util.choice, util.sequence
 trim = util.trim()
 blank = S" \t\n"
 clean = util.clean(blank)
+inspect = util.inspect
+
+tmpout = os.tmpname()
+tmperr = os.tmpname()
+
+pandoc_cmd = "pandoc --reference-links --verbose -f latex -t markdown_strict+tex_math_dollars-raw_html+simple_tables+smart -o " .. tmpout .. " 2> " .. tmperr
+preamble =[[
+\def\meta#1{⟨#1⟩}
+\def\cs#1{\\texttt{\\textbackslash #1}}
+\def\marg#1{\texttt{\{#1\}}}
+\def\oarg#1{\texttt{[#1]}}
+\def\declare#1{#1}
+\def\pageref#1{??}
+\def\opt#1{#1}
+\def\text#1{#1}
+\def\mvar#1{#1}
+\def\declareandlabel#1{\verb|#1|}
+\def\example{}
+\def\pgfname{PGF}
+\def\pdf{PDF}
+\def\makeatletter{}
+\def\tikzname{TikZ}
+\def\noindent{}
+\def\medskip{}
+\def\keyalias#1{}
+\def\ref#1{??}
+
+]]
+
+function pandockify(s)
+  local pd = io.popen(pandoc_cmd, "w")
+  pd:write(preamble)
+  pd:write(s)
+  local exit = pd:close()
+  local ef = io.open(tmperr)
+  local err = ef:read"all"
+--  if err ~= "" then print(err) end
+  ef:close()
+  return exit and io.open(tmpout):read"all", (err ~= "") and err
+end
+
+function deverbify(s)
+  s = replace("\\", "\\textbackslash ")(s)
+  s = replace("&", "\\& ")(s)
+  s = replace("_", "\\_ ")(s)
+  s = replace("#", "\\# ")(s)
+  s = replace("{", "\\{")(s)
+  s = replace("}", "\\}")(s)
+  return "\\texttt{" .. s .. "}"
+end
 
 function pipe(val, funs)
   for i = 1, #funs do
@@ -49,7 +93,7 @@ Pgroup = util.between_balanced("{", "}", "\\" * S"{}" + P(1))
 -- read main parts of the book
 
 interesting_envs = {"key", "command", "stylekey", "shape"}
-Pinteresting_envs = choice(unpack(interesting_envs))
+Pinteresting_envs = choice(table.unpack(interesting_envs))
 Pitem = Cp()*P"\\begin{" * C(Pinteresting_envs) * P"}" * Pgroup
 Pitems = Ct(search(Ct(Pitem))^0)
 skipping_examples = surrounded("\\begin{codeexample}", "\\end{codeexample}") + 1
@@ -59,9 +103,10 @@ Penvs = Ct(search(Ct(Penv))^0)
 
 items = {}
 
-for f in lfs.dir(arg[1] .. "doc/text-en") do
+
+for f in lfs.dir(arg[1]) do
   if f:match("%.tex$") then
-    local s = io.open(arg[1] .. "/doc/text-en/" .. f):read"all"
+    local s = io.open(arg[1] .. f):read"all"
     for _, v in ipairs(Pitems:match(s)) do
       local pos, kind, signature = v[1], v[2], v[3]
       local text = Penv:match(s, pos)
@@ -72,6 +117,16 @@ for f in lfs.dir(arg[1] .. "doc/text-en") do
                                    * search(")" * blank^0 * P(-1)), ""),
                          clean})
       if not items[kind] then items[kind] = {} end
+
+      text = replace(surrounded("|","|"), deverbify, skipping_examples)(text)
+      text = replace(C(cs"tikz" * Pgroup), "[PICTURE]", skipping_examples)(text)
+      text = replace(surrounded("\\tikz" * (1-Pletter), ";"), "[PICTURE]", skipping_examples)(text)
+      text = replace("\\begin{codeexample}" * surrounded("[","]"), "\\begin{verbatim}")(text)
+      text = replace("\\end{codeexample}", "\\end{verbatim}")(text)
+      text = replace("\\" * C(P"begin" + "end") * "{" * Pinteresting_envs * "}", "\\%1{comment}")(text)
+      -- local ntext, err = pandockify(text)
+      -- --if not ntext then print(err) end
+      -- if ntext and err then ntext = ntext .. "\n" .. err end
       items[kind][signature] = text
     end
   end
@@ -105,7 +160,8 @@ for sig, text in pairs(items.command) do
   local csname, args = sigpatt:match(sig)
   commands[csname] = {}
   if args and #args>0 then commands[csname].arguments = args end
-  commands[csname].documentation = "texdoc:generic/pgf/pgfmanual.pdf#pgf.<CS>" .. csname
+  commands[csname].documentation = "texmf:doc/generic/pgf/pgfmanual.pdf#pgf.back/" .. csname
+  commands[csname].details = pandockify(text)
 end
 
 -- treat keys
@@ -136,31 +192,24 @@ for sig, text in pairs(items.key) do
   local key, val = keypatt:match(sig)
   local res = {}
   if val then res.meta = val end
-  res.documentation = "texdoc:generic/pgf/pgfmanual.pdf#pgf." .. key:gsub(" ", ":")
+  res.documentation = "texmf:doc/generic/pgf/pgfmanual.pdf#pgf." .. key:gsub(" ", ":")
+  res.details = pandockify(text)
   keys[key] = res
 end
 
 comments = [[
-Extracted from the PGF manual
+Extracted from the PGF manual version 3.1.9a
 URL: https://github.com/pgf-tikz/pgf/
 Original license: GNU FDL v1.2+ or LPPL 1.3+
 ]]
 
-tikz = {comments = comments,
-        package = {
-          name = "tikz",
-          documentation = "texdoc:generic/pgf/pgfmanual.pdf"
-        },
-        commands = {},
-        keys = {tikz ={}, ['data visualization'] ={}, ['graphs'] = {}}
+tikz = {
+  commands = {},
+  keys = {tikz ={}, ['data visualization'] ={}, ['graphs'] = {}}
 }
-pgf = {comments = comments,
-       package = {
-         name = "pgf",
-         documentation = "generic/pgf/pgfmanual.pdf"
-       },
-       commands = {},
-       keys = {pgf ={}}
+pgf = {
+  commands = {},
+  keys = {pgf ={}}
 }
 
 for k,v in pairs(keys) do
@@ -186,7 +235,6 @@ tikzpathcommands = {
   "graph",
   "matrix",
   "node",
-  "nodepart",
   "pic",
   "path",
   "clip",
@@ -202,27 +250,46 @@ tikzpathcommands = {
   "useasboundingbox",
 }
 
-commands.path.arguments = {
-  {meta="specification", delimiters = {"",";"}}
-}
 for _, k in ipairs(tikzpathcommands) do
-  commands[k].arguments = merge(commands.path.arguments)
+  commands[k].arguments = {{meta="specification", delimiters = {"",";"}}}
   commands[k].action = 'tikzpath'
+  if k ~= "path" and util.nested_get(tikz, "keys", "tikz", k, "details") and commands[k].details then
+    commands[k].summary = clean(commands[k].details)
+    commands[k].details = tikz.keys.tikz[k].details
+  end
 end
 
 for cs, v in pairs(commands) do
   if cs:match'pgf' then
     pgf.commands[cs] = v
     for _, v in ipairs(v.arguments or {}) do
-      if v.meta == "options" then v.keys = "$DIGESTIFDATA/pgf/keys/pgf" end
+      if v.meta == "options" then v.keys = "$ref:pgf#/keys/pgf" end
     end
   else
     tikz.commands[cs] = v
     for _, v in ipairs(v.arguments or {}) do
-      if v.meta == "options" then v.keys = "$DIGESTIFDATA/tikz/keys/tikz" end
+      if v.meta == "options" then v.keys = "$ref:tikz#/keys/tikz" end
     end
   end
 end
 
-save_from_table('tikz.lua', tikz, {"comments", "package"})
-save_from_table('pgf.lua', pgf, {"comments", "package"})
+documentation = {
+  {
+    summary = "PGF Manual",
+    uri = "texmf:doc/generic/pgf/pgfmanual.pdf"
+  }
+}
+
+f = io.open("tikz.tags", "w")
+f:write("comments = ", inspect(comments), "\n")
+f:write("ctan_package = \"tikz\"\n")
+f:write("documentation = ", inspect(documentation), "\n")
+f:write("commands = ", inspect(tikz.commands), "\n")
+f:write("keys = ", inspect(tikz.keys), "\n")
+
+f = io.open("pgf.tags", "w")
+f:write("comments = ", inspect(comments), "\n")
+f:write("ctan_package = \"pgf\"\n")
+f:write("documentation = ", inspect(documentation), "\n")
+f:write("commands = ", inspect(pgf.commands), "\n")
+f:write("keys = ", inspect(pgf.keys), "\n")
