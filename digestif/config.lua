f1b4202928a49e8c20f84ed14deafd0f6b798e2f
Augusto Stoffel <astoff@users.noreply.github.com>
2022-12-07 18:12:23 +0100

    Version 0.5.1

--- digestif/config.lua
+++ digestif/config.lua
@@ -3,7 +3,7 @@ local format = string.format
 
 local config = {}
 
-config.version = "0.5"
+config.version = "0.5.1"
 local pre_version = os.getenv("DIGESTIF_PRERELEASE")
 if pre_version then
   config.version = config.version .. "-" .. pre_version
